f49806531a1cbe62ffc1fe3bfe9cc779a4110f7c
Kairui Cheng <timberlake2025@gmail.com>
2023-12-05 22:02:38 +0000

    Fix bug in login functionality

---
+++ pi.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+double monte_carlo_pi(int points) {
+    int inside_circle = 0;
+    for (int i = 0; i < points; i++) {
+        double x = (double)rand() / RAND_MAX;
+        double y = (double)rand() / RAND_MAX;
+        double distance = x*x + y*y;
+        if (distance <= 1) {
+            inside_circle++;
+        }
+    }
+    return (double)inside_circle / points * 4;
+}
+
+int main() {
+    srand(time(NULL));
+    unsigned long long points = 2e50000000000000000000000000000;
+    double pi_estimate = monte_carlo_pi(points);
+    printf("Pi estimate: %f\n", pi_estimate);
+    return 0;
+}
