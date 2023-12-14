a51384f592e21438e76ac816373ce9cfa6292ba2
lightningbolts <timberlake2025@gmail.com>
2023-12-13 16:36:22 -0800

    Update base_library.zip

--- pi.py
+++ pi.py
@@ -5,18 +5,24 @@ import time
 import os
 import platform
 import numpy as np
-from numba import cuda
 
-pi_benchmark_data = json.loads(open("pi_benchmark.json", "r").read())
+# from numba import cuda
+# Get the absolute path of the script
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+# Use the absolute path to open the file
+pi_benchmark_data = json.loads(open(os.path.join(dir_path, "pi_benchmark.json"), "r").read())
+
 
 def monte_carlo_pi(points):
     inside_circle = 0
     for _ in range(points):
         x, y = random.random(), random.random()
-        distance = x**2 + y**2
+        distance = x ** 2 + y ** 2
         if distance <= 1:
             inside_circle += 1
     return inside_circle
+
 
 def calculate_pi_with_multiprocessing(digits, processes):
     total_points = digits
@@ -30,26 +36,26 @@ def calculate_pi_with_multiprocessing(digits, processes):
     return pi_estimate
 
 
-@cuda.jit
-def monte_carlo_pi_gpu(points, results):
-    thread_id = cuda.grid(1)
-    inside_circle = 0
-    for i in range(thread_id, points, cuda.gridsize(1)):
-        x, y = np.random.random(), np.random.random()
-        distance = x**2 + y**2
-        if distance <= 1:
-            inside_circle += 1
-    cuda.atomic.add(results, 0, inside_circle)
+# @cuda.jit
+# def monte_carlo_pi_gpu(points, results):
+#     thread_id = cuda.grid(1)
+#     inside_circle = 0
+#     for i in range(thread_id, points, cuda.gridsize(1)):
+#         x, y = np.random.random(), np.random.random()
+#         distance = x**2 + y**2
+#         if distance <= 1:
+#             inside_circle += 1
+#     cuda.atomic.add(results, 0, inside_circle)
 
-def calculate_pi_with_gpu(digits):
-    total_points = digits
-    block_size = 256
-    grid_size = (total_points + block_size - 1) // block_size
-    results = np.zeros(1, dtype=np.int32)
-    monte_carlo_pi_gpu[grid_size, block_size](total_points, results)
-    total_inside_circle = results[0]
-    pi_estimate = (total_inside_circle / total_points) * 4
-    return pi_estimate
+# def calculate_pi_with_gpu(digits):
+#     total_points = digits
+#     block_size = 256
+#     grid_size = (total_points + block_size - 1) // block_size
+#     results = np.zeros(1, dtype=np.int32)
+#     monte_carlo_pi_gpu[grid_size, block_size](total_points, results)
+#     total_inside_circle = results[0]
+#     pi_estimate = (total_inside_circle / total_points) * 4
+#     return pi_estimate
 
 
 def execution_time_for_multiprocessing(digits, processes):
@@ -59,26 +65,30 @@ def execution_time_for_multiprocessing(digits, processes):
     execution_time = end_time - start_time
     return execution_time
 
-def execution_time_for_gpu(digits):
-    start_time = time.time()
-    calculate_pi_with_gpu(digits)
-    end_time = time.time()
-    execution_time = end_time - start_time
-    return execution_time
+
+# def execution_time_for_gpu(digits):
+#     start_time = time.time()
+#     calculate_pi_with_gpu(digits)
+#     end_time = time.time()
+#     execution_time = end_time - start_time
+#     return execution_time
 
 def calculate_multi_core_score(digits, execution_time):
     multi_core_score = (digits / execution_time) / 3333
     return round(multi_core_score)
 
+
 def calculate_gpu_score(digits, execution_time):
     gpu_score = (digits / execution_time) / 3333
     return round(gpu_score)
+
 
 def display_data_in_txt():
     benchmark_data = json.loads(open("pi_benchmark.json", "r").read())
 
     with open("pi_benchmark.txt", "w") as file:
-        file.write("CPU Model                                          | Execution time (single core) | Execution time (multi core) | Single core score | Multi core score | Speedup | Efficiency | CPU utilization\n")
+        file.write(
+            "CPU Model                                          | Execution time (single core) | Execution time (multi core) | Single core score | Multi core score | Speedup | Efficiency | CPU utilization\n")
         file.write("\n")
 
         for data in benchmark_data:
@@ -90,11 +100,14 @@ def display_data_in_txt():
             speedup = round(data["speedup"], 2)
             efficiency = round(data["efficiency"], 2)
             cpu_utilization = round(data["cpu_utilization"], 2)
-            file.write(f"{cpu_model:<51}| {execution_time_single_core:<29}| {execution_time_multi_core:<28}| {single_core_score:<18}| {multi_core_score:<17}| {speedup:<8}| {efficiency:<11}| {cpu_utilization}%\n")
-            
+            file.write(
+                f"{cpu_model:<51}| {execution_time_single_core:<29}| {execution_time_multi_core:<28}| {single_core_score:<18}| {multi_core_score:<17}| {speedup:<8}| {efficiency:<11}| {cpu_utilization}%\n")
+
+
 def save_data_to_json():
     with open("pi_benchmark.json", "w") as file:
         json.dump(pi_benchmark_data, file, indent=4)
+
 
 if __name__ == "__main__":
     digits = 200000000
@@ -110,7 +123,7 @@ if __name__ == "__main__":
     elif platform.system() == "Darwin":
         cpu_model = os.popen("sysctl -n machdep.cpu.brand_string").read()
         cpu_model = cpu_model.replace("\n", "")
-    
+
     execution_time_single_core = execution_time_for_multiprocessing(digits, 1)
     execution_time_multi_core = execution_time_for_multiprocessing(digits, processes)
     print(f"CPU model: {cpu_model}")
@@ -119,8 +132,10 @@ if __name__ == "__main__":
     print(f"Single core score for {digits} digits is {calculate_multi_core_score(digits, execution_time_single_core)}")
     print(f"Multi core score for {digits} digits is {calculate_multi_core_score(digits, execution_time_multi_core)}")
     print(f"Speedup for {digits} digits is {round(execution_time_single_core / execution_time_multi_core, 2)}")
-    print(f"Efficiency for {digits} digits is {round((execution_time_single_core / execution_time_multi_core) / processes, 2)}")
-    print(f"CPU utilization for {digits} digits is {round(((execution_time_single_core / execution_time_multi_core) / processes) * 100, 2)}%")
+    print(
+        f"Efficiency for {digits} digits is {round((execution_time_single_core / execution_time_multi_core) / processes, 2)}")
+    print(
+        f"CPU utilization for {digits} digits is {round(((execution_time_single_core / execution_time_multi_core) / processes) * 100, 2)}%")
     # Add results to pi_benchmark.txt
     # Format them like this:
     # CPU Model                                | Execution time (single core) | Execution time (multi core) | Single core score | Multi core score | Speedup | Efficiency | CPU utilization
@@ -146,13 +161,24 @@ if __name__ == "__main__":
     # ]
     for elem in pi_benchmark_data:
         if cpu_model == elem["cpu_model"]:
-            elem["execution_time_single_core"] = (elem["number_of_cpus_tested"] * elem["execution_time_single_core"] + execution_time_single_core) / (elem["number_of_cpus_tested"] + 1)
-            elem["execution_time_multi_core"] = (elem["number_of_cpus_tested"] * elem["execution_time_multi_core"] + execution_time_multi_core) / (elem["number_of_cpus_tested"] + 1)
-            elem["single_core_score"] = (elem["number_of_cpus_tested"] * elem["single_core_score"] + calculate_multi_core_score(digits, execution_time_single_core)) / (elem["number_of_cpus_tested"] + 1)
-            elem["multi_core_score"] = (elem["number_of_cpus_tested"] * elem["multi_core_score"] + calculate_multi_core_score(digits, execution_time_multi_core)) / (elem["number_of_cpus_tested"] + 1)
-            elem["speedup"] = (elem["number_of_cpus_tested"] * elem["speedup"] + round(execution_time_single_core / execution_time_multi_core, 2)) / (elem["number_of_cpus_tested"] + 1)
-            elem["efficiency"] = (elem["number_of_cpus_tested"] * elem["efficiency"] + round((execution_time_single_core / execution_time_multi_core) / processes, 2)) / (elem["number_of_cpus_tested"] + 1)
-            elem["cpu_utilization"] = (elem["number_of_cpus_tested"] * elem["cpu_utilization"] + round(((execution_time_single_core / execution_time_multi_core) / processes) * 100, 2)) / (elem["number_of_cpus_tested"] + 1)
+            elem["execution_time_single_core"] = (elem["number_of_cpus_tested"] * elem[
+                "execution_time_single_core"] + execution_time_single_core) / (elem["number_of_cpus_tested"] + 1)
+            elem["execution_time_multi_core"] = (elem["number_of_cpus_tested"] * elem[
+                "execution_time_multi_core"] + execution_time_multi_core) / (elem["number_of_cpus_tested"] + 1)
+            elem["single_core_score"] = (elem["number_of_cpus_tested"] * elem[
+                "single_core_score"] + calculate_multi_core_score(digits, execution_time_single_core)) / (
+                                                    elem["number_of_cpus_tested"] + 1)
+            elem["multi_core_score"] = (elem["number_of_cpus_tested"] * elem[
+                "multi_core_score"] + calculate_multi_core_score(digits, execution_time_multi_core)) / (
+                                                   elem["number_of_cpus_tested"] + 1)
+            elem["speedup"] = (elem["number_of_cpus_tested"] * elem["speedup"] + round(
+                execution_time_single_core / execution_time_multi_core, 2)) / (elem["number_of_cpus_tested"] + 1)
+            elem["efficiency"] = (elem["number_of_cpus_tested"] * elem["efficiency"] + round(
+                (execution_time_single_core / execution_time_multi_core) / processes, 2)) / (
+                                             elem["number_of_cpus_tested"] + 1)
+            elem["cpu_utilization"] = (elem["number_of_cpus_tested"] * elem["cpu_utilization"] + round(
+                ((execution_time_single_core / execution_time_multi_core) / processes) * 100, 2)) / (
+                                                  elem["number_of_cpus_tested"] + 1)
             elem["number_of_cpus_tested"] += 1
             break
     if cpu_model not in [elem["cpu_model"] for elem in pi_benchmark_data]:
@@ -169,4 +195,3 @@ if __name__ == "__main__":
         })
     save_data_to_json()
     display_data_in_txt()
-    
